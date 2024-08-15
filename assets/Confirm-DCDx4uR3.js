cb117527c20d3248cd48e2a3c32763597631ce52
codingbooth3 <codingbooth3@users.noreply.github.com>
2024-08-15 20:48:41 +0000

    deploy: 7325b6856c063402a4e587e2b648818723e7f6d1

---
+++ assets/Confirm-DCDx4uR3.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-GOTFym5Y.js";function n(){return r.jsx("div",{children:"Confirm"})}export{n as default};
---
+++ assets/palette-7xt8Zjuu.js
@@ -0,0 +1,1 @@
+const s="/booth-cosmetics/assets/foundation-D8N8L0TO.jpg",t="/booth-cosmetics/assets/lipbalm-BYMMrfSQ.jpg",o="/booth-cosmetics/assets/palette-DIqfgKvx.jpg";export{s as f,t as l,o as p};
