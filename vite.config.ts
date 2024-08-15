0af5562aa442717f5847a3b3ec89cd43565c8018
Chris <157486296+codingbooth3@users.noreply.github.com>
2024-08-15 19:52:22 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'booth-cosmetics',
+  base: '/booth-cosmetics/',
   define: {
     'process.env': {}
   }
